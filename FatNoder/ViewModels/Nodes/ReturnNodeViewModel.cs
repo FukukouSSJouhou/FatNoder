728d4477e9d5989177c9c1c6fc750f97952bf454
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 07:15:28 +0900

    added base class

--- FatNoder/ViewModels/Nodes/ReturnNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/ReturnNodeViewModel.cs
@@ -1,4 +1,8 @@
-﻿using NodeNetwork.ViewModels;
+﻿using DynamicData;
+using NodeNetwork.Toolkit.ValueNode;
+using NodeNetwork.ViewModels;
+using NodeNetwork.Views;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -7,8 +11,23 @@ using System.Threading.Tasks;
 
 namespace FatNoder.ViewModels.Nodes
 {
-    public class ReturnNodeViewModel<T>:NodeViewModel
+    public class ReturnNodeViewModel<T> : NodeViewModel
     {
+        static ReturnNodeViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<ReturnNodeViewModel<T>>));
 
+        }
+        public ValueNodeInputViewModel<T?> ReturnInput { get; }
+        public ReturnNodeViewModel()
+        {
+            Name = "Return";
+            this.CanBeRemovedByUser = false;
+            ReturnInput = new ValueNodeInputViewModel<T?>
+            {
+                Name = "Value"
+            };
+            this.Inputs.Add(ReturnInput);
+        }
     }
 }
