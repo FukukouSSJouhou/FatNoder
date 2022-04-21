db99f4973851f76b29024606f769f50a646d0ce0
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 07:08:56 +0900

    added tc

---
+++ FatNoder/ViewModels/Nodes/ReturnNodeViewModel.cs
@@ -0,0 +1,14 @@
+﻿using NodeNetwork.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels.Nodes
+{
+    public class ReturnNodeViewModel<T>:NodeViewModel
+    {
+
+    }
+}
