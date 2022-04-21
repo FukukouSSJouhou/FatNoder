f0bc5fab4d6b44ab2f26beae8ab8deed130b4795
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 00:51:39 +0900

    fx

--- FatNoder/MainWindow.xaml.cs
+++ FatNoder/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DynamicData;
+using NodeNetwork.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -23,6 +25,33 @@ namespace FatNoder
         public MainWindow()
         {
             InitializeComponent();
+            //Create a new viewmodel for the NetworkView
+            var network = new NetworkViewModel();
+
+            //Create the node for the first node, set its name and add it to the network.
+            var node1 = new NodeViewModel();
+            node1.Name = "Node 1";
+            network.Nodes.Add(node1);
+
+            //Create the viewmodel for the input on the first node, set its name and add it to the node.
+            var node1Input = new NodeInputViewModel();
+            node1Input.Name = "Node 1 input";
+            var node1Input2 = new NodeInputViewModel();
+            node1Input2.Name = "Node 1 input2";
+            node1.Inputs.Add(node1Input);
+            node1.Inputs.Add(node1Input2);
+
+            //Create the second node viewmodel, set its name, add it to the network and add an output in a similar fashion.
+            var node2 = new NodeViewModel();
+            node2.Name = "Node 2";
+            network.Nodes.Add(node2);
+
+            var node2Output = new NodeOutputViewModel();
+            node2Output.Name = "Node 2 output";
+            node2.Outputs.Add(node2Output);
+
+            //Assign the viewmodel to the view.
+            networkView.ViewModel = network;
         }
     }
 }
