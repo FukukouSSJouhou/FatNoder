0068d85571ece3edf9bed7b7d8d26ddff82259ca
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 07:23:58 +0900

    added

---
+++ FatNoder/ViewModels/HannyouValueEditorViewModel.cs
@@ -0,0 +1,40 @@
+﻿using FatNoder.Views;
+using NodeNetwork.Toolkit.ValueNode;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels
+{
+    public class HannyouValueEditorViewModel<T>:ValueEditorViewModel<T>
+    {
+        static HannyouValueEditorViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new HannyouValueEditorView(), typeof(IViewFor<HannyouValueEditorViewModel<T>>));
+
+        }
+        public HannyouValueEditorViewModel()
+        {
+            object ret = null;
+            if (typeof(T) == typeof(string))
+            {
+                ret = "";
+            }
+            else if(typeof(T)==typeof(int))
+            {
+                ret = 0;
+            }else if (typeof(T) == typeof(long))
+            {
+                ret = 0;
+            }
+            else
+            {
+                ret = null;
+            }
+            Value=(T)ret;
+        }
+    }
+}
