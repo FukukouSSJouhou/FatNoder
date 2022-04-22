e1311f29868d43fd23b3d6206405bbfdb6134935
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 23:38:04 +0900

    a4f

---
+++ FatNoder/ViewModels/CoderOutputViewModel.cs
@@ -0,0 +1,27 @@
+﻿using FatNoder.Model.TransC;
+using NodeNetwork.Toolkit.ValueNode;
+using NodeNetwork.Views;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels
+{
+    public class CoderOutputViewModel<T> : ValueNodeOutputViewModel<T>
+    {
+        static CoderOutputViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new NodeOutputView(), typeof(IViewFor<CoderOutputViewModel<T>>));
+        }
+        public CoderOutputViewModel(Type StructData)
+        {
+            if (StructData == typeof(IStateMent))
+            {
+                this.PortPosition = NodeNetwork.ViewModels.PortPosition.Left;
+            }
+        }
+    }
+}
