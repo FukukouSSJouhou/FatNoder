1966efd5356284e4e1eb2ff07e20676983bfe6fa
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 22:06:39 +0900

    added

---
+++ FatNoder/ViewModels/MainViewModel.cs
@@ -0,0 +1,58 @@
+﻿using DynamicData;
+using NodeNetwork.Toolkit.BreadcrumbBar;
+using NodeNetwork.Toolkit.NodeList;
+using NodeNetwork.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reactive;
+using System.Reactive.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels
+{
+    class NetworkBreadcrumb : BreadcrumbViewModel
+    {
+        #region Network
+        private NetworkViewModel _network;
+        public NetworkViewModel Network
+        {
+            get => _network;
+            set => this.RaiseAndSetIfChanged(ref _network, value);
+        }
+        #endregion
+    }
+    public class MainViewModel:ReactiveObject
+    {
+        #region Network
+        private readonly ObservableAsPropertyHelper<NetworkViewModel> _network;
+        public NetworkViewModel Network => _network.Value;
+        #endregion
+
+        public BreadcrumbBarViewModel NetworkBreadcrumbBar { get; } = new BreadcrumbBarViewModel();
+        public NodeListViewModel NodeList { get; } = new NodeListViewModel();
+
+        public ReactiveCommand<Unit, Unit> AutoLayout { get; }
+        public ReactiveCommand<Unit, Unit> StartAutoLayoutLive { get; }
+        public ReactiveCommand<Unit, Unit> StopAutoLayoutLive { get; }
+
+        public ReactiveCommand<Unit, Unit> GroupNodes { get; }
+        public ReactiveCommand<Unit, Unit> UngroupNodes { get; }
+        public ReactiveCommand<Unit, Unit> OpenGroup { get; }
+        public MainViewModel()
+        {
+            this.WhenAnyValue(vm => vm.NetworkBreadcrumbBar.ActiveItem).Cast<NetworkBreadcrumb>()
+                .Select(b => b?.Network)
+                .ToProperty(this, vm => vm.Network, out _network);
+            NetworkBreadcrumbBar.ActivePath.Add(new NetworkBreadcrumb
+            {
+                Name = "Main",
+                Network = new NetworkViewModel()
+            });
+
+        }
+
+    }
+}
