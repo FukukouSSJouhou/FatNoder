1837d87ea492b25004e18f401d7ce9499b8a5de4
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 18:22:47 +0900

    a

---
+++ FatNoder/Views/StringValueEditorView.xaml.cs
@@ -0,0 +1,38 @@
+﻿using FatNoder.ViewModels;
+using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace FatNoder.Views
+{
+    /// <summary>
+    /// StringValueEditorView.xaml の相互作用ロジック
+    /// </summary>
+    public partial class StringValueEditorView : UserControl,IViewFor<HannyouValueEditorViewModel>
+    {
+        #region ViewModel
+        public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(nameof(ViewModel),
+            typeof(HannyouValueEditorViewModel<string>), typeof(StringValueEditorView), new PropertyMetadata(null));
+        public HannyouValueEditorViewModel<string> ViewModel
+        {
+            get => (HannyouValueEditorViewModel<string>)GetValue(ViewModelProperty);
+            set => SetValue(ViewModelProperty, value);
+        }
+        public StringValueEditorView()
+        {
+            InitializeComponent();
+        }
+    }
+}
