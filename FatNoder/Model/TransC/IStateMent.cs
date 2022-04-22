0fd0d6e371df026cf372691cf34ef2cd8de76731
kahenteikou <kokkiemouse@gmail.com>
2022-04-22 23:26:28 +0900

    n

--- FatNoder/Model/TransC/IStateMent.cs
+++ FatNoder/Model/TransC/IStateMent.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NodeNetwork.ViewModels;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace FatNoder.Model.TransC
 {
     public interface IStateMent
     {
+        public NodeViewModel ChiNode { get; set; }
     }
 }
