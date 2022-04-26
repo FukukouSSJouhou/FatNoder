a24c4065bd456aaf31e3ae38ae46cc8136362d5f
kahenteikou <kokkiemouse@gmail.com>
2022-04-26 17:03:16 +0900

    a

---
+++ FatNoder/ViewModels/Nodes/PrintNodeViewModel.cs
@@ -0,0 +1,23 @@
+﻿using FatNoder.Serializer.Node.Xml;
+using FatNoder.ViewModels.Xml;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels.Nodes
+{
+    public class PrintNodeViewModel : StatementNodeViewModelBase, INVModelXML
+    {
+        public XmlNodeDatas GetXMLNodeDT()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void SetXMLNodeDT(XmlNodeDatas xmldt)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
