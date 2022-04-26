30b326d2c3d689830ca6d6fdd4b4842e4b8d6cfb
kahenteikou <kokkiemouse@gmail.com>
2022-04-26 22:37:02 +0900

    a

--- NodeAyano/NodeAyano.cs
+++ NodeAyano/NodeAyano.cs
@@ -1,10 +1,19 @@
-﻿namespace NodeAyano
+﻿using FatNoder.Serializer.Node.Xml;
+
+namespace NodeAyano
 {
     /// <summary>
     /// This is a trans compiler.
     /// </summary>
     public class NodeAyano
     {
-        public NodeAyano()
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="xmlr">xml rootnode</param>
+        public NodeAyano(XmlRootN xmlr)
+        {
+
+        }
     }
 }
