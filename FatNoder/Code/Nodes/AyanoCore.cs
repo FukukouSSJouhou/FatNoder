65f203534175e90580a7212683ed5fabf8ddeb73
kahenteikou <kokkiemouse@gmail.com>
2022-04-26 22:43:42 +0900

    added

---
+++ FatNoder/Code/Nodes/AyanoCore.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.Code.Nodes
+{
+    /// <summary>
+    /// This is a transcompiler.
+    /// </summary>
+    public class AyanoCore
+    {
+    }
+}
