837ec3b10d73bf8f0464b847b0e3357a9d67dd71
kahenteikou <kokkiemouse@gmail.com>
2022-04-27 07:17:32 +0900

    iter

--- FatNoder/ViewModels/Enumerators/NodeViewModelEnumerator.cs
+++ FatNoder/ViewModels/Enumerators/NodeViewModelEnumerator.cs
@@ -1,4 +1,7 @@
-﻿using NodeNetworkJH.ViewModels;
+﻿using DynamicData;
+using FatNoder.Model.Transc;
+using NodeNetworkJH.Toolkit.ValueNode;
+using NodeNetworkJH.ViewModels;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -10,16 +13,59 @@ namespace FatNoder.ViewModels.Enumerators
 {
     public class NodeViewModelEnumerator : IEnumerator
     {
-        public object Current => throw new NotImplementedException();
+        private NodeViewModel _currentNVM;
+        private NodeViewModel _RootNVM;
+        private IEnumerable<NodeViewModel> _nodes;
+        public object Current
+        {
+            get
+            {
+                return _currentNVM;
+            }
+        }
 
         public bool MoveNext()
         {
-            throw new NotImplementedException();
+            if (_currentNVM == null)
+            {
+                _currentNVM = _RootNVM;
+                return true;
+            }else
+            {
+
+                foreach (NodeInputViewModel nvi in _currentNVM.Inputs.Items)
+                {
+                    if (nvi.Name == "Out")
+                    {
+                        dynamic dyi = nvi;
+
+                        if (dyi is ValueListNodeInputViewModel<StatementCls>)
+                        {
+                            IObservableList<StatementCls> valueskun;
+
+                            valueskun = dyi.Values as IObservableList<StatementCls>;
+
+                            foreach (StatementCls s in valueskun.Items)
+                            {
+                                foreach (NodeViewModel n in _nodes)
+                                {
+                                    if (n.UUID == s.UUID)
+                                    {
+                                        _currentNVM = n;
+                                        return true;
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                return false;
+            }
         }
 
         public void Reset()
         {
-            throw new NotImplementedException();
+            _currentNVM = null;
         }
     }
 }
