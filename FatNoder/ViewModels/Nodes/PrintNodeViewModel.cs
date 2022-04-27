97daef07ecbb55ff721ac1189549767a60f0a90c
kahenteikou <kokkiemouse@gmail.com>
2022-04-27 22:56:00 +0900

    fx

--- FatNoder/ViewModels/Nodes/PrintNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/PrintNodeViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using DynamicData;
+using FatNoder.Model.Transc;
 using FatNoder.Serializer.Node.Xml;
+using NodeAyano.Model.Nodes;
 using NodeNetworkJH.Toolkit.ValueNode;
 using NodeNetworkJH.Views;
 using ReactiveUI;
@@ -18,9 +20,19 @@ namespace FatNoder.ViewModels.Nodes
         {
             Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<PrintNodeViewModel>));
         }
+        
         public ValueNodeInputViewModel<string?> PrintInput { get; }
+        private PrintNodeModel _model = new PrintNodeModel();
+        public PrintNodeModel model
+        {
+            get
+            {
+                return _model;
+            }
+        }
         public PrintNodeViewModel()
         {
+            model.TYPE = typeof(PrintNodeViewModel).ToString();
 
             PrintInput = new ValueNodeInputViewModel<string?>
             {
@@ -30,7 +42,44 @@ namespace FatNoder.ViewModels.Nodes
             };
             PrintInput.ValueChanged.Subscribe(newvalue =>
             {
-                Debug.Print("Set print : " + newvalue);
+                model.Value = newvalue;
+            });
+
+            this.UUIDChanged.Subscribe(newvalue =>
+            {
+                model.UUID = newvalue;
+            });
+            this.NameChanged.Subscribe(newvalue =>
+            {
+                model.Name = newvalue;
+            });
+            this.PositionChanged.Subscribe(newvalue =>
+            {
+                model.Points = new XMLNodeXY()
+                {
+                    X = newvalue.X,
+                    Y = newvalue.Y
+                };
+            });
+            model.InputStates = new XMLNodeInputStatement_VMLS();
+            model.InputStates.Add(new XMLNodeInputStatement()
+            {
+                States = new XMLNodeInputStatementLS(),
+                Name = InputFlow.Name
+            });
+            this.WhenAnyObservable(vm => vm.InputFlow.Values.CountChanged).Subscribe(newvalue =>
+            {
+                foreach (XMLNodeInputStatement xs in model.InputStates.Where(d =>
+                {
+                    return d.Name == InputFlow.Name;
+                }))
+                {
+                    xs.States = new XMLNodeInputStatementLS();
+                    foreach (StatementCls guidkun in InputFlow.Values.Items)
+                    {
+                        xs.States.Add(guidkun.UUID);
+                    }
+                }
             });
             this.Inputs.Add(PrintInput);
         }
