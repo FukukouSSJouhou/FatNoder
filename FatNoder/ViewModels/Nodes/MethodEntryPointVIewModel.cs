c0836af0d0f1a57c6e5574dc31a2719572cb1f26
kahenteikou <kokkiemouse@gmail.com>
2022-04-27 20:42:30 +0900

    added

--- FatNoder/ViewModels/Nodes/MethodEntryPointVIewModel.cs
+++ FatNoder/ViewModels/Nodes/MethodEntryPointVIewModel.cs
@@ -1,5 +1,6 @@
 ﻿using DynamicData;
 using FatNoder.Model.Transc;
+using NodeAyano.Model.Nodes;
 using NodeNetworkJH.Toolkit.ValueNode;
 using NodeNetworkJH.ViewModels;
 using NodeNetworkJH.Views;
@@ -21,15 +22,41 @@ namespace FatNoder.ViewModels.Nodes
         }
 
         public ValueListNodeInputViewModel<StatementCls> Input { get; }
+        private MethodEntryPoint _model=new MethodEntryPoint();
+        public MethodEntryPoint model
+        {
+            get
+            {
+                return _model;
+            }
+        }
         public MethodEntryPointVIewModel()
         {
+            model.TYPE = typeof(MethodEntryPointVIewModel).ToString();
 
+            this.UUIDChanged.Subscribe(newvalue =>
+            {
+                model.UUID = newvalue;
+            });
+            this.NameChanged.Subscribe(newvalue =>
+            {
+                model.Name = newvalue;
+            });
+            this.PositionChanged.Subscribe(newvalue =>
+            {
+                model.Points = new XMLNodeXY()
+                {
+                    X = newvalue.X,
+                    Y = newvalue.Y
+                };
+            });
             Input = new CoderListInputViewModel<StatementCls>(typeof(StatementCls))
             {
                 Name = "statement",
                 PortPosition=PortPosition.Right,
                 MaxConnections = 1
             };
+
             
             this.Inputs.Add(Input);
         }
