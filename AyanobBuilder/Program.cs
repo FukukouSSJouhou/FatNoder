7d4ee5e3919fde0cb0e196428292c9fe6a47adba
kahenteikou <kokkiemouse@gmail.com>
2022-04-28 21:23:08 +0900

    added

---
+++ AyanobBuilder/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace FatNoder
+{
+    public class AyanoBuilder
+    {
+        public static int Main(string[] args)
+        {
+            Console.WriteLine("TDN");
+            Console.ReadKey();
+            return 0;
+        }
+    }
+}
