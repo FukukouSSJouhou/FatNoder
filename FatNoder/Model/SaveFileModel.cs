61e1e1990a39ef872b7aa9e0da505ca637c2a03f
kahenteikou <kokkiemouse@gmail.com>
2022-04-30 17:04:21 +0900

    added model

--- FatNoder/Model/SaveFileModel.cs
+++ FatNoder/Model/SaveFileModel.cs
@@ -1,10 +1,12 @@
 ﻿using FatNoder.Serializer.Node.Xml;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace FatNoder.Model
 {
@@ -13,6 +15,47 @@ namespace FatNoder.Model
     /// </summary>
     public class SaveFileModel
     {
+        /// <summary>
+        /// Save XML File 
+        /// </summary>
+        /// <param name="req">Request</param>
+        /// <returns>Response</returns>
+        public SaveFileResponse SaveXML(SaveFileRequest req)
+        {
+            SaveFileResponse resp = null;
+            try
+            {
+                using (StreamWriter sw = new StreamWriter(req.FilePath, false))
+                {
+
+                    var settings = new XmlWriterSettings()
+                    {
+                        Indent = true,
+                        IndentChars = "    ",
+                        Encoding = new System.Text.UTF8Encoding(false)
+                    };
+
+                    using (var xw = XmlWriter.Create(sw, settings))
+                    {
+                        req.Serializer.WriteObject(xw, req.RootXML);
+                    }
+                }
+                resp = new SaveFileResponse()
+                {
+                    IsSucceed = true,
+                    Message = "XML saved successfully!"
+                };
+            }
+            catch (Exception ex)
+            {
+                resp = new SaveFileResponse()
+                {
+                    IsSucceed = false,
+                    Message = ex.Message
+                };
+            }
+            return resp;
+        }
         /// <summary>
         /// Save File Request Class
         /// </summary>
