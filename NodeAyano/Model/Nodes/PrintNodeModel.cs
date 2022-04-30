6c968dbc69daf1eaea33985c6fdaa69215fabfe9
kahenteikou <kokkiemouse@gmail.com>
2022-04-30 13:54:18 +0900

    added2

--- NodeAyano/Model/Nodes/PrintNodeModel.cs
+++ NodeAyano/Model/Nodes/PrintNodeModel.cs
@@ -1,4 +1,5 @@
 ﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,7 +12,7 @@ namespace NodeAyano.Model.Nodes
     /// <summary>
     /// Print Node Model
     /// </summary>
-    public class PrintNodeModel: XML_NodeModel
+    public class PrintNodeModel: CompileNodeBase
     {
 
         [DataMember(Name = "Value", Order = 8)]
@@ -19,6 +20,16 @@ namespace NodeAyano.Model.Nodes
         public string Value
         {
             get; set;
+        }
+        [DataMember(Name = "isconnected", Order = 9)]
+        public bool Isconnected
+        {
+            get; set;
+        }
+
+        public override StatementSyntax[] CompileSyntax()
+        {
+            throw new NotImplementedException();
         }
     }
 }
