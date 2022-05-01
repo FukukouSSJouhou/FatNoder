8d4d0ebacba08b2c54b9a598cac4d38400a6de54
kahenteikou <kokkiemouse@gmail.com>
2022-05-01 22:54:54 +0900

    p3

--- AyanobBuilder/compilers/CUIProgram.cs
+++ AyanobBuilder/compilers/CUIProgram.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using AyanoBuilder.CUItools;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,6 +19,8 @@ namespace AyanoBuilder.compilers
         /// <returns>Exit Code</returns>
         public static int MainCUI(string[] args)
         {
+            ConsoleWrapper.BluePrint("[AyanoBuilder]");
+
             return 0;
         }
     }
