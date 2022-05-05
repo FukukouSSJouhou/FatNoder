cc75b3198739ddac6248511f1f851829749a6917
kahenteikou <kokkiemouse@gmail.com>
2022-05-06 06:50:57 +0900

    a2

--- PluginManagerXML/PluginManagerXML.cs
+++ PluginManagerXML/PluginManagerXML.cs
@@ -2,6 +2,9 @@
 {
     public class PluginManagerXML
     {
-
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ModelDll { get; set; }
+        public string ViewModelDLL { get; set; }
     }
 }
