a68468428f0bccfd8f8bf2a63a3b705d0a55d953
kahenteikou <kokkiemouse@gmail.com>
2022-05-07 23:23:18 +0900

    23

--- FatNoder/ViewModels/CSCodePreviewViewModel.cs
+++ FatNoder/ViewModels/CSCodePreviewViewModel.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reactive.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,5 +13,28 @@ namespace FatNoder.ViewModels
     /// </summary>
     public class CSCodePreviewViewModel : ReactiveObject
     {
+        /// <summary>
+        /// preview code
+        /// </summary>
+        public string Code
+        {
+            get => _code;
+            set => this.RaiseAndSetIfChanged(ref _code, value);
+        }
+        private string _code;
+        private readonly ObservableAsPropertyHelper<string> _outCode;
+        /// <summary>
+        /// Out Code
+        /// </summary>
+        public string OutCode => _outCode.Value;
+
+        public CSCodePreviewViewModel()
+        {
+            this.WhenAnyValue(vm => vm.Code).Where(c => c != null)
+                .Select(c =>
+                {
+                    return c;
+                }).ToProperty(this, vm => vm.OutCode, out _outCode);
+        }
     }
 }
