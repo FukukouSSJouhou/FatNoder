21cff66849b521bb90c8e1acf2efeccc056279b8
kahenteikou <kokkiemouse@gmail.com>
2022-05-07 23:14:19 +0900

    added x m l

--- FatNoder/ViewModels/CSCodePreviewViewModel.cs
+++ FatNoder/ViewModels/CSCodePreviewViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,6 +7,9 @@ using System.Threading.Tasks;
 
 namespace FatNoder.ViewModels
 {
+    /// <summary>
+    /// Preview VM!
+    /// </summary>
     public class CSCodePreviewViewModel : ReactiveObject
     {
     }
