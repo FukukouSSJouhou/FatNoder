0fb0fa65d4aefc44fe7074fd24febcc4b3299e62
kahenteikou <kokkiemouse@gmail.com>
2022-05-12 07:21:14 +0900

    f2

--- NodeAyano/NodeAyanoCompiler.cs
+++ NodeAyano/NodeAyanoCompiler.cs
@@ -414,11 +414,25 @@ namespace NodeAyano
             {
                 var emitResult = compilation.Emit(stream);
 
-                foreach (var diagnostic in emitResult.Diagnostics)
+                foreach (var d in emitResult.Diagnostics)
                 {
-                    var pos = diagnostic.Location.GetLineSpan();
+                    var pos = d.Location.GetLineSpan();
                     var location = "(" + pos.Path + "@Line" + (pos.StartLinePosition.Line + 1) + ":" + (pos.StartLinePosition.Character + 1) + ")";
-                    Console.WriteLine($"[{diagnostic.Severity}, {location}] {diagnostic.Id}, {diagnostic.GetMessage()}");
+                    switch (d.Severity)
+                    {
+                        case DiagnosticSeverity.Warning:
+                            Console.WriteLine($"\u001b[38;2;255;255;0m[{d.Severity}, {location}] {d.Id}, {d.GetMessage()}\u001b[0m");
+                            break;
+                        case DiagnosticSeverity.Error:
+                            Console.WriteLine($"\u001b[38;2;255;0;0m[{d.Severity}, {location}] {d.Id}, {d.GetMessage()}\u001b[0m");
+                            break;
+                        case DiagnosticSeverity.Hidden:
+                            Console.WriteLine($"\u001b[38;2;255;0;255m[{d.Severity}, {location}] {d.Id}, {d.GetMessage()}\u001b[0m");
+                            break;
+                        default:
+                            Console.WriteLine($"[{d.Severity}, {location}] {d.Id}, {d.GetMessage()}");
+                            break;
+                    }
                 }
 
                 if (!emitResult.Success)
