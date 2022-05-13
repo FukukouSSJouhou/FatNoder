545de9fcfe7be8502a48bfc935c1591dc4dcf46c
kahenteikou <kokkiemouse@gmail.com>
2022-05-13 23:33:46 +0900

    a2d

--- AyanoPluginCommonAPI/Attributes/AyanoNodeModelClassAttribute.cs
+++ AyanoPluginCommonAPI/Attributes/AyanoNodeModelClassAttribute.cs
@@ -6,11 +6,29 @@ using System.Threading.Tasks;
 
 namespace AyanoPluginCommonAPI.Attributes
 {
+    /// <summary>
+    /// Model Class Attribute
+    /// </summary>
     public class AyanoNodeModelClassAttribute :Attribute
     {
+        /// <summary>
+        /// Model name
+        /// </summary>
         public string ModelName { get; set; }
+        /// <summary>
+        /// model category
+        /// </summary>
         public string ModelCategory { get; set; }
+        /// <summary>
+        /// model description
+        /// </summary>
         public string ModelDescription { get; set; }
+        /// <summary>
+        /// constructor
+        /// </summary>
+        /// <param name="modelName">name</param>
+        /// <param name="modelCategory">category</param>
+        /// <param name="modelDescription">description</param>
         public AyanoNodeModelClassAttribute(string modelName, string modelCategory, string modelDescription):base()
         {
             ModelName = modelName;
