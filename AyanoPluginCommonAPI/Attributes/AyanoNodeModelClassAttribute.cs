438c44908da2da0420555628f3d89675cd3ae204
kahenteikou <kokkiemouse@gmail.com>
2022-05-13 23:31:30 +0900

    added cls

---
+++ AyanoPluginCommonAPI/Attributes/AyanoNodeModelClassAttribute.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanoPluginCommonAPI.Attributes
+{
+    public class AyanoNodeModelClassAttribute :Attribute
+    {
+        public string ModelName { get; set; }
+        public string ModelCategory { get; set; }
+        public string ModelDescription { get; set; }
+        public AyanoNodeModelClassAttribute(string modelName, string modelCategory, string modelDescription):base()
+        {
+            ModelName = modelName;
+            ModelCategory = modelCategory;
+            ModelDescription = modelDescription;
+        }
+    }
+}
