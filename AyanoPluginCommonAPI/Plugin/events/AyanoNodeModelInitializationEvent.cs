c17e410ac1f4ba927c8c175ae7444782ae06ce3b
kahenteikou <kokkiemouse@gmail.com>
2022-05-16 07:06:05 +0900

    added event

---
+++ AyanoPluginCommonAPI/Plugin/events/AyanoNodeModelInitializationEvent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AyanoPluginCommonAPI.Plugin.events
+{
+    /// <summary>
+    /// Initialization event
+    /// </summary>
+    public class AyanoNodeModelInitializationEvent
+    {
+    }
+}
