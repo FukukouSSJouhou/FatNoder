a6b2d5317aacb03e950af35a135a0f57f1e6be92
kahenteikou <kokkiemouse@gmail.com>
2022-05-16 07:02:55 +0900

    a2

--- AyanoPluginCommonAPI/Attributes/AyanoNodeModelClassAttribute.cs
+++ AyanoPluginCommonAPI/Attributes/AyanoNodeModelClassAttribute.cs
@@ -35,5 +35,13 @@ namespace AyanoPluginCommonAPI.Attributes
             Category = category;
             Description = description;
         }
+        /// <summary>
+        /// Initializer Attribute
+        /// </summary>
+        [AttributeUsage(AttributeTargets.Method)]
+        public sealed class InitializerAttribute : Attribute
+        {
+
+        }
     }
 }
