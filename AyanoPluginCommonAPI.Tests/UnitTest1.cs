11584015aa416c6edafcf8eff8d5aa363a3eae62
kahenteikou <kokkiemouse@gmail.com>
2022-05-27 07:19:41 +0900

    s

--- AyanoPluginCommonAPI.Tests/UnitTest1.cs
+++ AyanoPluginCommonAPI.Tests/UnitTest1.cs
@@ -40,15 +40,24 @@ namespace AyanoPluginCommonAPI.Tests
                         {
                             foreach(var m in clsType.GetMethods())
                             {
-                                var tattr = m.GetCustomAttributes(typeof(Attributes.AyanoNodeModelClassAttribute.InitializerAttribute));
-                                if (tattr == null || tattr.Count() == 0) continue;
-                                
+                                var tattrs = m.GetCustomAttributes(typeof(Attributes.AyanoNodeModelClassAttribute.InitializerAttribute));
+                                if (tattrs == null || tattrs.Count() == 0) continue;
+                                var names2 = new List<String>();
+                                foreach(var tattr in tattrs)
+                                {
+                                    var pCls = tattr as Attributes.AyanoNodeModelClassAttribute.InitializerAttribute;
+                                    if(pCls != null)
+                                    {
+                                        names.Add(m.Name);
+                                    }
+                                }
                             }
                         }
                         methods.Add(new PlugInMethod(clsType, names));
                     }
                 }
             }
+            Console.WriteLine("TDN");
             Assert.Pass();
         }
     }
