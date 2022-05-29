a720b1e33d639f4a327a3f9391fab1df0deb8144
kahenteikou <kokkiemouse@gmail.com>
2022-05-30 07:16:34 +0900

    a2

--- AyanoPluginCommonAPI.Tests/UnitTest1.cs
+++ AyanoPluginCommonAPI.Tests/UnitTest1.cs
@@ -18,14 +18,13 @@ namespace AyanoPluginCommonAPI.Tests
                 this.MethodNames = methods;
             }
         }
-        [Test]
-        public void Test1()
+        public static List<PlugInMethod> Get_PluginModel_Methods()
         {
-            var methods = new List< PlugInMethod>();
-            var asm=Assembly.GetAssembly(typeof(AyanoPluginCommonAPI.Tests.TestModelClass));
+            var methods = new List<PlugInMethod>();
+            var asm = Assembly.GetAssembly(typeof(AyanoPluginCommonAPI.Tests.TestModelClass));
             {
                 var clsTypes = asm.GetTypes();
-                foreach(var clsType in clsTypes)
+                foreach (var clsType in clsTypes)
                 {
                     var typeattrs = clsType.GetCustomAttributes(typeof(Attributes.AyanoNodeModelClassAttribute));
                     if (typeattrs == null || typeattrs.Count() == 0)
@@ -33,22 +32,22 @@ namespace AyanoPluginCommonAPI.Tests
                         continue;
                     }
                     var names = new List<String>();
-                    foreach(var typeattr in typeattrs)
+                    foreach (var typeattr in typeattrs)
                     {
                         var pluginClass = typeattr as Attributes.AyanoNodeModelClassAttribute;
-                        if(pluginClass != null)
+                        if (pluginClass != null)
                         {
-                            foreach(var m in clsType.GetMethods())
+                            foreach (var m in clsType.GetMethods())
                             {
                                 var tattrs = m.GetCustomAttributes(typeof(Attributes.AyanoNodeModelClassAttribute.InitializerAttribute));
                                 if (tattrs == null || tattrs.Count() == 0) continue;
                                 var names2 = new List<String>();
-                                foreach(var tattr in tattrs)
+                                foreach (var tattr in tattrs)
                                 {
                                     var pCls = tattr as Attributes.AyanoNodeModelClassAttribute.InitializerAttribute;
-                                    if(pCls != null)
+                                    if (pCls != null)
                                     {
-                                        if(m.GetGenericArguments().Length != 0) continue;
+                                        if (m.GetGenericArguments().Length != 0) continue;
                                         names.Add(m.Name);
                                     }
                                 }
@@ -57,6 +56,20 @@ namespace AyanoPluginCommonAPI.Tests
                         methods.Add(new PlugInMethod(clsType, names));
                     }
                 }
+            }
+            return methods;
+
+        }
+        [Test]
+        public void Test1()
+        {
+            Plugin.events.AyanoNodeModelInitializationEvent evkun = new Plugin.events.AyanoNodeModelInitializationEvent();
+            evkun.ModelRegistered += (sender,evar)=>{
+                Assert.That(evar.modeltype, Is.EqualTo(typeof(string)));
+            };
+            foreach(var met in Get_PluginModel_Methods())
+            {
+
             }
             Console.WriteLine("TDN");
             Assert.Pass();
