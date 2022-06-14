04fa718ac64b557cd524fd976fe8de363f002a3a
kahenteikou <kokkiemouse@gmail.com>
2022-06-15 07:12:47 +0900

    23

--- rcbuilder/RCBuilder.cs
+++ rcbuilder/RCBuilder.cs
@@ -2,6 +2,10 @@
 {
     public class RCBuilder:Microsoft.Build.Utilities.Task
     {
+        [Microsoft.Build.Framework.Required]
+        public Microsoft.Build.Framework.ITaskItem RCFile { get; set; }
+        [Microsoft.Build.Framework.Output]
+        public string OutResFile { get; set; }
         public override bool Execute()
         {
             return true;
