5227664cb09803351d866d3a1f2a707569421dc0
kahenteikou <kokkiemouse@gmail.com>
2022-06-26 22:05:22 +0900

    22

---
+++ NodeAyano/Model/Nodes/ValueCompileNodeBase.cs
@@ -0,0 +1,21 @@
+﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeAyano.Model.Nodes
+{
+    public abstract class ValueCompileNodeBase : XML_NodeModel
+    {
+        /// <summary>
+        /// コンパイラメソッド
+        /// </summary>
+        /// <param name="xnodes">node list</param>
+        /// <returns>メンバー君</returns>
+        public abstract LiteralExpressionSyntax CompileSyntax(IEnumerable<XML_NodeModel> xnodes);
+
+    }
+}
