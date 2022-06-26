9115864b0e14a22204b92363d07d9660e44ba3db
kahenteikou <kokkiemouse@gmail.com>
2022-06-26 20:25:57 +0900

    23x

--- FatNoder/ViewModels/Nodes/InputNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/InputNodeViewModel.cs
@@ -1,13 +1,41 @@
-﻿using NodeAyanoVMLibs.ViewModels.Nodes;
+﻿using NodeAyano.Model.Nodes;
+using NodeAyanoVMLibs.ViewModels.Nodes;
+using NodeNetworkJH.Toolkit.ValueNode;
+using NodeNetworkJH.Views;
+using ReactiveUI;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AyanoNodeVM;
+using FatNoder.Serializer.Node.Xml;
+using System.Xml.Linq;
 
 namespace FatNoder.ViewModels.Nodes
 {
-    public partial class InputNodeViewModel<T>: INodeViewModelBase
+    public partial class InputNodeViewModel<T>: NodeVMBasekun,INodeViewModelBase
     {
+        static InputNodeViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<InputNodeViewModel<T>>));
+        }
+        public HannyouValueEditorViewModel<T> ValueEditor { get; } = new HannyouValueEditorViewModel<T>();
+        [ModelAyano]
+        private InputNodeModel<T> _model = new InputNodeModel<T>();
+        public ValueNodeOutputViewModel<T?> Output { get; }
+
+        public void ChangeStates(XML_NodeModel newmodelbs)
+        {
+
+            Name = newmodelbs.Name;
+            Position = new System.Windows.Point
+            {
+                X = newmodelbs.Points.X,
+                Y = newmodelbs.Points.Y
+            };
+            ValueEditor.Value = ((InputNodeModel<T>)newmodelbs).Value;
+        }
+
     }
 }
