3158123cb3b42bb9981dcb1370982717c8b36631
kahenteikou <kokkiemouse@gmail.com>
2022-06-26 20:23:55 +0900

    bunkatu zissou

--- FatNoder/ViewModels/Nodes/InputNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/InputNodeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NodeAyanoVMLibs.ViewModels.Nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace FatNoder.ViewModels.Nodes
 {
-    internal class InputNodeViewModel
+    public partial class InputNodeViewModel<T>: INodeViewModelBase
     {
     }
 }
