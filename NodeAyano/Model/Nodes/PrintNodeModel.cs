77d84a532967a6cb79da4f93fefd964c264df4b6
kahenteikou <kokkiemouse@gmail.com>
2022-06-27 00:03:14 +0900

    fx2

--- NodeAyano/Model/Nodes/PrintNodeModel.cs
+++ NodeAyano/Model/Nodes/PrintNodeModel.cs
@@ -58,7 +58,7 @@ namespace NodeAyano.Model.Nodes
 
                                                 ) }
 
-                                        )
+                                    )
                             ))));
                             }
                         }
