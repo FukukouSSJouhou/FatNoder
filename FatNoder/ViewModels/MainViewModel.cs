632d2d2a3857c216a475d982b2141d5f690f6ea1
kahenteikou <kokkiemouse@gmail.com>
2022-06-28 07:22:45 +0900

    p2

--- FatNoder/ViewModels/MainViewModel.cs
+++ FatNoder/ViewModels/MainViewModel.cs
@@ -115,6 +115,7 @@ namespace FatNoder.ViewModels
             NodeList.AddNodeType(() => new InputNodeViewModel<int> { Name = "IntInput" });
             NodeList.AddNodeType(() => new InputNodeViewModel<string> { Name = "StringInput" });
             NodeList.AddNodeType(() => new SetValueNodeViewModel<int> { Name = "IntSetValue" });
+            NodeList.AddNodeType(() => new GetValueNodeViewModel<int> { Name = "IntGetValue" });
             this.WhenAnyObservable(vm => vm.Network.NetworkChanged).Subscribe(newvalue =>
             {
                 List<Type> typelistkun = new List<Type>();
