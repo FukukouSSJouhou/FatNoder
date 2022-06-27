33e11cf308998a72d992a09172a08a38ef4a200d
kahenteikou <kokkiemouse@gmail.com>
2022-06-27 22:57:08 +0900

    2x

--- FatNoder/ViewModels/Nodes/SetValueNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/SetValueNodeViewModel.cs
@@ -11,6 +11,9 @@ using System.Threading.Tasks;
 using AyanoNodeVM;
 using FatNoder.Serializer.Node.Xml;
 using NodeNetworkJH.Toolkit.ValueNode;
+using DynamicData;
+using FatNoder.Model.Transc;
+using NodeNetworkJH.ViewModels;
 
 namespace FatNoder.ViewModels.Nodes
 {
@@ -27,11 +30,139 @@ namespace FatNoder.ViewModels.Nodes
         public SetValueNodeViewModel(Guid uuid) : base(uuid)
         {
             InitAyanoVMB();
+            HensuuInput = new ValueNodeInputViewModel<T?>
+            {
+                Name = "Value",
+                MaxConnections = 1
+            };
+            HensuuInput.ValueChanged.Subscribe(newvalue =>
+            {
+                _model.Value = newvalue;
+            });
+
+            _model.Inputs = new XMLNodeInputS
+            {
+                new XMLNodeInput()
+                {
+                    Name = HensuuInput.Name,
+                    connections=new XMLNodeInputConnectS
+                    {
+
+                    }
+                }
+            };
+            _model.InputStates = new XMLNodeInputStatement_VMLS();
+            _model.InputStates.Add(new XMLNodeInputStatement()
+            {
+                States = new XMLNodeInputStatementLS(),
+                Name = InputFlow.Name
+            });
+            this.WhenAnyObservable(vm => vm.InputFlow.Values.CountChanged).Subscribe(newvalue =>
+            {
+                foreach (XMLNodeInputStatement xs in _model.InputStates.Where(d =>
+                {
+                    return d.Name == InputFlow.Name;
+                }))
+                {
+                    xs.States.Clear();
+                    foreach (StatementCls guidkun in InputFlow.Values.Items)
+                    {
+                        xs.States.Add(guidkun.UUID);
+                    }
+                }
+            });
+            HensuuInput.Connections.CountChanged.Subscribe(newvalue =>
+            {
+                foreach (XMLNodeInput xs in _model.Inputs.Where(d =>
+                {
+                    return d.Name == HensuuInput.Name;
+                }))
+                {
+                    xs.connections.Clear();
+                    foreach (ConnectionViewModel cv in HensuuInput.Connections.Items)
+                    {
+                        //Console.WriteLine($"{cv.Input.Name},{cv.Input.Parent.UUID}");
+                        xs.connections.Add(
+                            new XMLNodeInputConnect
+                            {
+                                Name = cv.Output.Name,
+                                Target = cv.Output.Parent.UUID,
+                                InputOnly = true
+                            });
+                    }
+                }
+
+            });
+            this.Inputs.Add(HensuuInput);
         }
         ///<inheritdoc/>
         public SetValueNodeViewModel() : base()
         {
             InitAyanoVMB();
+            HensuuInput = new ValueNodeInputViewModel<T?>
+            {
+                Name = "Value",
+                MaxConnections = 1
+            };
+            HensuuInput.ValueChanged.Subscribe(newvalue =>
+            {
+                _model.Value = newvalue;
+            });
+
+            _model.Inputs = new XMLNodeInputS
+            {
+                new XMLNodeInput()
+                {
+                    Name = HensuuInput.Name,
+                    connections=new XMLNodeInputConnectS
+                    {
+
+                    }
+                }
+            };
+            _model.InputStates = new XMLNodeInputStatement_VMLS();
+            _model.InputStates.Add(new XMLNodeInputStatement()
+            {
+                States = new XMLNodeInputStatementLS(),
+                Name = InputFlow.Name
+            });
+            this.WhenAnyObservable(vm => vm.InputFlow.Values.CountChanged).Subscribe(newvalue =>
+            {
+                foreach (XMLNodeInputStatement xs in _model.InputStates.Where(d =>
+                {
+                    return d.Name == InputFlow.Name;
+                }))
+                {
+                    xs.States.Clear();
+                    foreach (StatementCls guidkun in InputFlow.Values.Items)
+                    {
+                        xs.States.Add(guidkun.UUID);
+                    }
+                }
+            });
+            HensuuInput.Connections.CountChanged.Subscribe(newvalue =>
+            {
+                foreach (XMLNodeInput xs in _model.Inputs.Where(d =>
+                {
+                    return d.Name == HensuuInput.Name;
+                }))
+                {
+                    xs.connections.Clear();
+                    foreach (ConnectionViewModel cv in HensuuInput.Connections.Items)
+                    {
+                        //Console.WriteLine($"{cv.Input.Name},{cv.Input.Parent.UUID}");
+                        xs.connections.Add(
+                            new XMLNodeInputConnect
+                            {
+                                Name = cv.Output.Name,
+                                Target = cv.Output.Parent.UUID,
+                                InputOnly = true
+                            });
+                    }
+                }
+
+            });
+            this.Inputs.Add(HensuuInput);
         }
         ///<inheritdoc/>
         public void ChangeStates(XML_NodeModel newmodelbs)
