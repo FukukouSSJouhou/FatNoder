1e7592d2bf4afad847a4aba8d5e8f1dbea58f1d6
kahenteikou <kokkiemouse@gmail.com>
2022-06-27 23:28:46 +0900

    2d

--- NodeAyano/Model/Nodes/GetValueNodeModel.cs
+++ NodeAyano/Model/Nodes/GetValueNodeModel.cs
@@ -1,12 +1,35 @@
-﻿using System;
+﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NodeAyano.Model.Nodes
 {
-    public class GetValueNodeModel<T>
+    /// <summary>
+    /// Get Value Node Model
+    /// </summary>
+    /// <typeparam name="T">Type</typeparam>
+    public class GetValueNodeModel<T> : ValueCompileNodeBase
     {
+
+        /// <summary>
+        /// Value Name
+        /// </summary>
+        [DataMember(Name = "ValueName", Order = 8)]
+
+        public string ValueName
+        {
+            get; set;
+        }
+        ///<inheritdoc/>
+        public override LiteralExpressionSyntax CompileSyntax(IEnumerable<XML_NodeModel> xnodes)
+        {
+
+        }
     }
 }
