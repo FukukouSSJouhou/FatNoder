ae3cf35a465e6b37a6882d06f38aa9303007b73f
kahenteikou <kokkiemouse@gmail.com>
2022-06-28 07:08:52 +0900

    2

--- FatNoder/ViewModels/Nodes/GetValueNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/GetValueNodeViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using NodeNetworkJH.Views;
+using ReactiveUI;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,9 @@ namespace FatNoder.ViewModels.Nodes
 {
     public partial class GetValueNodeViewModel<T> : StatementNodeViewModelBase, INodeViewModelBase
     {
+        static GetValueNodeViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(()=>new NodeView(),typeof(IViewFor<GetValueNodeViewModel<T>>));
+        }
     }
 }
