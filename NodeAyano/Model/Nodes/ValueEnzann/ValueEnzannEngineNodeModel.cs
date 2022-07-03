9629d0e90cfe46b30f60c57fbbd18c54da0fcb8e
kahenteikou <kokkiemouse@gmail.com>
2022-07-03 16:48:32 +0900

    j

--- NodeAyano/Model/Nodes/ValueEnzann/ValueEnzannEngineNodeModel.cs
+++ NodeAyano/Model/Nodes/ValueEnzann/ValueEnzannEngineNodeModel.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.CodeAnalysis.CSharp;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Runtime.Serialization;
@@ -33,7 +36,12 @@ namespace NodeAyano.Model.Nodes.ValueEnzann
         /// Subtract
         /// </summary>
         [EnumMember]
-        Subtract
+        Subtract,
+        /// <summary>
+        /// None
+        /// </summary>
+        [EnumMember]
+        None
     }
     public class ValueEnzannEngineNodeModel : ValueCompileNodeBase
     {
@@ -52,6 +60,70 @@ namespace NodeAyano.Model.Nodes.ValueEnzann
         public ValueEnzannEngineType CalcType
         {
             get;set;
+        }
+        public ValueEnzannEngineNodeModel()
+        {
+            CalcType = ValueEnzannEngineType.None;
+        }
+
+
+        /// <inheritdoc/>
+
+        public override ExpressionSyntax CompileSyntax(IEnumerable<XML_NodeModel> xnodes)
+        {
+            ValueCompileNodeBase input1 = null;
+            ValueCompileNodeBase input2 = null;
+            foreach (XMLNodeInput xnode in Inputs)
+            {
+                if (xnode.Name == "Input1")
+                {
+
+                    foreach (XMLNodeInputConnect cn in xnode.connections)
+                    {
+                        foreach (XML_NodeModel modelkun in xnodes.Where(
+                            d =>
+                            {
+                                return d.UUID == cn.Target;
+                            }))
+                        {
+                            if (modelkun is ValueCompileNodeBase)
+                            {
+                                input1 = (ValueCompileNodeBase)modelkun;
+
+                            }
+                        }
+                    }
+                }
+                else if (xnode.Name == "Input2")
+                {
+
+                    foreach (XMLNodeInputConnect cn in xnode.connections)
+                    {
+                        foreach (XML_NodeModel modelkun in xnodes.Where(
+                            d =>
+                            {
+                                return d.UUID == cn.Target;
+                            }))
+                        {
+                            if (modelkun is ValueCompileNodeBase)
+                            {
+                                input2 = (ValueCompileNodeBase)modelkun;
+
+                            }
+                        }
+                    }
+                }
+            }
+            if (input1 != null && input2 != null)
+            {
+                SyntaxKind ensankind = SyntaxKind.None;
+                return SyntaxFactory.BinaryExpression(SyntaxKind.AddExpression, input1.CompileSyntax(xnodes), input2.CompileSyntax(xnodes));
+            }
+            else
+            {
+                return SyntaxFactory.LiteralExpression(SyntaxKind.NumericLiteralExpression, SyntaxFactory.Literal(
+                                        0));
+            }
         }
     }
 }
