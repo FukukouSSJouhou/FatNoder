5ee22b37d314e7162defd15169980eed7a900632
kahenteikou <kokkiemouse@gmail.com>
2022-07-04 07:20:58 +0900

    a

--- FatNoder/Plugins/PluginLoader.cs
+++ FatNoder/Plugins/PluginLoader.cs
@@ -8,5 +8,11 @@ namespace FatNoder.Plugins
 {
     public class PluginLoader
     {
+        public void Load_Plugins()
+        {
+            string application_dir = System.AppDomain.CurrentDomain.BaseDirectory;
+            string plugins_dir = System.IO.Path.Combine(application_dir, "plugins");
+
+        }
     }
 }
