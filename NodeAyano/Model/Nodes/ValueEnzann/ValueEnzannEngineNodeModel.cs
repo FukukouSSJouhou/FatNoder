c241cacc7f401b401ee870c6688ecd24daf0a22f
kahenteikou <kokkiemouse@gmail.com>
2022-07-03 16:39:39 +0900

    2

--- NodeAyano/Model/Nodes/ValueEnzann/ValueEnzannEngineNodeModel.cs
+++ NodeAyano/Model/Nodes/ValueEnzann/ValueEnzannEngineNodeModel.cs
@@ -1,12 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NodeAyano.Model.Nodes.ValueEnzann
 {
-    public class ValueEnzannEngineNodeModel
+    public enum ValueEnzannEngineType
     {
+        Add,
+        Divide,
+        Multiply,
+        Subract
+    }
+    public class ValueEnzannEngineNodeModel : ValueCompileNodeBase
+    {
+
+
+        [DataMember(Name = "Value", Order = 8)]
+
+        public string Value
+        {
+            get; set;
+        }
     }
 }
