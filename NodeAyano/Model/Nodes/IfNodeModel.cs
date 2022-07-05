a875ffbeaecea8d503a1a2870dac25dd89859798
kahenteikou <kokkiemouse@gmail.com>
2022-07-06 07:19:31 +0900

    2

--- NodeAyano/Model/Nodes/IfNodeModel.cs
+++ NodeAyano/Model/Nodes/IfNodeModel.cs
@@ -1,13 +1,30 @@
-﻿using System;
+﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NodeAyano.Model.Nodes
 {
     public class IfNodeModel : CompileNodeBase
     {
 
+        [DataMember(Name = "Value", Order = 8)]
+
+        public string Value
+        {
+            get; set;
+        }
+
+        /// <inheritdoc/>
+
+        public override StatementSyntax[] CompileSyntax(IEnumerable<XML_NodeModel> xnodes)
+        {
+
+        }
     }
 }
