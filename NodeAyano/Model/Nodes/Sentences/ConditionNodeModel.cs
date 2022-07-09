db148d2d4999dc26399c02bedf03c65c37665e31
kahenteikou <kokkiemouse@gmail.com>
2022-07-09 22:50:13 +0900

    a2

--- NodeAyano/Model/Nodes/Sentences/ConditionNodeModel.cs
+++ NodeAyano/Model/Nodes/Sentences/ConditionNodeModel.cs
@@ -1,12 +1,41 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Serialization;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace NodeAyano.Model.Nodes.Sentences
 {
-    public class ConditionNodeModel
+    public enum ConditionParamTypeEnum
     {
+        Equals,
+        GreaterThan,
+        LessThan,
+        LessThanOrEqual,
+        GreaterThanOrEqual,
+        NotEquals
+    }
+    public class ConditionNodeModel: ValueCompileNodeBase
+    {
+
+        [DataMember(Name = "Value", Order = 8)]
+        public string Value
+        {
+            get; set;
+        }
+        public ConditionParamTypeEnum ConditionType
+        {
+            get;set;
+        }
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public ConditionNodeModel()
+        {
+            ConditionType = ConditionParamTypeEnum.Equals;
+        }
+        
     }
 }
