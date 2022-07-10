626db167ec8b84ccc3d40665e5a4f690f8e98267
kahenteikou <kokkiemouse@gmail.com>
2022-07-10 21:19:32 +0900

    b

--- FatNoder/ViewModels/Nodes/Sentences/ConditionNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/Sentences/ConditionNodeViewModel.cs
@@ -10,6 +10,10 @@ using NodeNetworkJH.Views;
 using ReactiveUI;
 using NodeAyano.HensuuV;
 using NodeNetworkJH.Toolkit.ValueNode;
+using DynamicData;
+using FatNoder.Serializer.Node.Xml;
+using NodeNetworkJH.ViewModels;
+using FatNoder.ViewModels.Nodes.Sentences.Editors;
 
 namespace FatNoder.ViewModels.Nodes.Sentences
 {
@@ -32,5 +36,172 @@ namespace FatNoder.ViewModels.Nodes.Sentences
         public ValueNodeInputViewModel<HensuuUkewatashi?> Input2 { get; }
         public ValueNodeInputViewModel<ConditionParamTypeEnum?> ConditionTypeInput { get; }
 
+        public ConditionNodeViewModel(Guid uuid) : base(uuid)
+        {
+            InitAyanoVMB();
+            _model.InputOnly = true;
+            Output = new ValueNodeOutputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Value",
+                Label = "Value",
+                Value = this.WhenAnyValue(vm => vm.hkun)
+            };
+            Input1 = new ValueNodeInputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Input1",
+                Label = "Input1",
+                MaxConnections = 1
+            };
+            Input2 = new ValueNodeInputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Input2",
+                Label = "Input2",
+                MaxConnections = 1
+            };
+            ConditionTypeInput = new ValueNodeInputViewModel<ConditionParamTypeEnum?>
+            {
+                Name = "ConditionType",
+                Label = "Condition Type",
+                MaxConnections = 1
+
+            };
+            InitConstructor();
+        }
+        public ConditionNodeViewModel() : base()
+        {
+
+            InitAyanoVMB();
+            _model.InputOnly = true;
+            Output = new ValueNodeOutputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Value",
+                Label = "Value",
+                Value = this.WhenAnyValue(vm => vm.hkun)
+            };
+            Input1 = new ValueNodeInputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Input1",
+                Label = "Input1",
+                MaxConnections = 1
+            };
+            Input2 = new ValueNodeInputViewModel<HensuuUkewatashi?>
+            {
+                Name = "Input2",
+                Label = "Input2",
+                MaxConnections = 1
+            };
+            ConditionTypeInput = new ValueNodeInputViewModel<ConditionParamTypeEnum?>
+            {
+                Name = "ConditionType",
+                Label = "Condition Type",
+                MaxConnections = 1
+
+            };
+            InitConstructor();
+        }
+        private void InitConstructor()
+        {
+
+            ConditionTypeInput.ValueChanged.Subscribe(newvalue =>
+            {
+                if (newvalue != null)
+                    _model.ConditionType = newvalue.Value;
+            });
+            ConditionTypeInput.Editor = new ConditionNodeTypeEditorViewModel();
+            _model.Outputs = new XMLNodeOutputS
+            {
+                new XMLNodeOutput()
+                {
+                    Name = Output.Name,
+                    connections=new XMLNodeOutputConnectS
+                    {
+
+                    }
+                }
+            };
+
+            _model.Inputs = new XMLNodeInputS
+            {
+                new XMLNodeInput()
+                {
+                    Name = Input1.Name,
+                    connections=new XMLNodeInputConnectS
+                    {
+
+                    }
+                },
+                new XMLNodeInput()
+                {
+                    Name = Input2.Name,
+                    connections=new XMLNodeInputConnectS
+                    {
+
+                    }
+                }
+            };
+            Input1.Connections.CountChanged.Subscribe(newvalue =>
+            {
+
+                foreach (XMLNodeInput xs in _model.Inputs.Where(d =>
+                {
+                    return d.Name == Input1.Name;
+                }))
+                {
+                    xs.connections.Clear();
+                    foreach (ConnectionViewModel cv in Input1.Connections.Items)
+                    {
+                        //Console.WriteLine($"{cv.Input.Name},{cv.Input.Parent.UUID}");
+                        xs.connections.Add(
+                            new XMLNodeInputConnect
+                            {
+                                Name = cv.Output.Name,
+                                Target = cv.Output.Parent.UUID,
+                                InputOnly = true
+                            });
+                    }
+                }
+            });
+            Input2.Connections.CountChanged.Subscribe(newvalue =>
+            {
+
+                foreach (XMLNodeInput xs in _model.Inputs.Where(d =>
+                {
+                    return d.Name == Input2.Name;
+                }))
+                {
+                    xs.connections.Clear();
+                    foreach (ConnectionViewModel cv in Input2.Connections.Items)
+                    {
+                        //Console.WriteLine($"{cv.Input.Name},{cv.Input.Parent.UUID}");
+                        xs.connections.Add(
+                            new XMLNodeInputConnect
+                            {
+                                Name = cv.Output.Name,
+                                Target = cv.Output.Parent.UUID,
+                                InputOnly = true
+                            });
+                    }
+                }
+            });
+            this.Outputs.Add(Output);
+            this.Inputs.Add(ConditionTypeInput);
+            this.Inputs.Add(Input1);
+            this.Inputs.Add(Input2);
+            ConditionTypeInput.Port.IsVisible = false;
+
+        }
+        ///<inheritdoc/>
+        public void ChangeStates(XML_NodeModel newmodelbs)
+        {
+
+            Name = Properties.Resources.MainViewModel_Calc;
+            Position = new System.Windows.Point
+            {
+                X = newmodelbs.Points.X,
+                Y = newmodelbs.Points.Y
+            };
+            _model.ConditionType = ((ConditionNodeModel)newmodelbs).ConditionType;
+            ((ConditionNodeTypeEditorViewModel)ConditionTypeInput.Editor).SelectViewKun(_model.ConditionType);
+        }
     }
 }
