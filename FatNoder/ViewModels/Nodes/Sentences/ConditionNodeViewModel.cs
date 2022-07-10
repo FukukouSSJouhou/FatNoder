a282cec463e8c2e03dc02e0d18edaed1af18a5a4
kahenteikou <kokkiemouse@gmail.com>
2022-07-10 21:05:22 +0900

    a2

--- FatNoder/ViewModels/Nodes/Sentences/ConditionNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/Sentences/ConditionNodeViewModel.cs
@@ -1,12 +1,25 @@
-﻿using System;
+﻿using NodeAyano.Model.Nodes.Sentences;
+using NodeAyanoVMLibs.ViewModels.Nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using AyanoNodeVM;
+using FatNoder.ViewModels.Nodes.EnzanNodes;
+using NodeNetworkJH.Views;
+using ReactiveUI;
 
 namespace FatNoder.ViewModels.Nodes.Sentences
 {
-    public class ConditionNodeViewModel
+    public partial class ConditionNodeViewModel : NodeVMBasekun, INodeViewModelBase
     {
+
+        [ModelAyano]
+        private ConditionNodeModel _model = new ConditionNodeModel();
+        static ConditionNodeViewModel()
+        {
+            Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<ConditionNodeViewModel>));
+        }
     }
 }
