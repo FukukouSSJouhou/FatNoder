63c0f97917c1ad4d002fea2c6ffad094e34a09fc
kahenteikou <kokkiemouse@gmail.com>
2022-07-12 07:17:36 +0900

    2

---
+++ FatNoder/ViewModels/Ports/NodePortViewModel.cs
@@ -0,0 +1,30 @@
+﻿using NodeNetworkJH.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FatNoder.ViewModels.Ports
+{
+    /// <summary>
+    /// Port Type
+    /// </summary>
+    public enum PortType
+    {
+        /// <summary>
+        /// Statement
+        /// </summary>
+        Statement,
+        /// <summary>
+        /// Valuable
+        /// </summary>
+        Valuable
+    }
+    /// <summary>
+    /// Port VM
+    /// </summary>
+    public class NodePortViewModel:PortViewModel
+    {
+    }
+}
