4f7f7fd80e6d692ab850dde24f9cc87373ec924e
kahenteikou <kokkiemouse@gmail.com>
2022-07-20 06:57:13 +0900

    2

--- NodeAyano/Model/Nodes/VariableDeclarationCompileNodeBase.cs
+++ NodeAyano/Model/Nodes/VariableDeclarationCompileNodeBase.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using FatNoder.Serializer.Node.Xml;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace NodeAyano.Model.Nodes
 {
-    internal class VariableDeclarationCompileNodeBase
+    public abstract class VariableDeclarationCompileNodeBase : XML_NodeModel
     {
     }
 }
