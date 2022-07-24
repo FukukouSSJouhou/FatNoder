26492d398b0af70c8263e0b3cf5c75fe848b2a97
kahenteikou <kokkiemouse@gmail.com>
2022-07-24 17:09:33 +0900

    a

---
+++ NodeAyano/Model/Nodes/basepac/IExpressionSyntaxCompileNodeModelBase.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NodeAyano.Model.Nodes.basepac
+{
+    public interface IExpressionSyntaxCompileNodeModelBase
+    {
+    }
+}
