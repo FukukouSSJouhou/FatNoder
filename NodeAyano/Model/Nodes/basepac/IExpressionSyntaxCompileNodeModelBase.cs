d4d408be3248bf1b9a54f184010e8ea2fd930b4c
kahenteikou <kokkiemouse@gmail.com>
2022-07-24 17:18:30 +0900

    v2

--- NodeAyano/Model/Nodes/basepac/IExpressionSyntaxCompileNodeModelBase.cs
+++ NodeAyano/Model/Nodes/basepac/IExpressionSyntaxCompileNodeModelBase.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FatNoder.Serializer.Node.Xml;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +8,17 @@ using System.Threading.Tasks;
 
 namespace NodeAyano.Model.Nodes.basepac
 {
+    /// <summary>
+    /// ExpressionSyntax Compile Node Interface
+    /// </summary>
     public interface IExpressionSyntaxCompileNodeModelBase
     {
+
+        /// <summary>
+        /// コンパイラメソッド(変数???用)
+        /// </summary>
+        /// <param name="xnodes">xnodes</param>
+        /// <returns>ExpressionSyntax</returns>
+        public abstract ExpressionSyntax CompileSyntax_ExpressionS(IEnumerable<XML_NodeModel> xnodes);
     }
 }
