ac9ca14ee50000a87480b4fbf903197fc2e9f5ed
kahenteikou <kokkiemouse@gmail.com>
2022-12-17 18:48:13 +0900

    y

--- FatNoder/ViewModels/Nodes/Http/HttpClientPostDataNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/Http/HttpClientPostDataNodeViewModel.cs
@@ -1,7 +1,9 @@
 ﻿using FatNoder.Serializer.Node.Xml;
+using NodeAyano.HensuuV;
 using NodeAyano.Model.Nodes;
 using NodeAyano.Model.Nodes.Http;
 using NodeAyanoVMLibs.ViewModels.Nodes;
+using NodeNetworkJH.Toolkit.ValueNode;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace FatNoder.ViewModels.Nodes.Http
     {
 
         private HttpClientPostDataNodeModel _model = new HttpClientPostDataNodeModel();
+        public ValueNodeInputViewModel<HensuuUkewatashi?> InputURL {get;}
         /// <inheritdoc/>
 
         public void ChangeStates(XML_NodeModel newmodelbs)
