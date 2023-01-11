35fe93bcf11d3aef2c4a543f68ddda22b75e60b9
kahenteikou <kokkiemouse@gmail.com>
2023-01-12 07:15:27 +0900

    X

--- FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
@@ -10,12 +10,15 @@ using System.Threading.Tasks;
 using AyanoNodeVM;
 using NodeNetworkJH.Toolkit.ValueNode;
 using NodeAyano.HensuuV;
+using FatNoder.Model.Transc;
 
 namespace FatNoder.ViewModels.Nodes
 {
     public partial class ForNodeViewModel:StatementNodeViewModelBase,INodeViewModelBase
     {
         private ValueNodeInputViewModel<HensuuUkewatashi?> Condition { get; }
+        public ValueListNodeInputViewModel<StatementCls> DefineFor { get; }
+        public ValueListNodeInputViewModel<StatementCls> OutFor { get; }
         static ForNodeViewModel()
         {
             Splat.Locator.CurrentMutable.Register(() => new NodeView(), typeof(IViewFor<ForNodeViewModel>));
