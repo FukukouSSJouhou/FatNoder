7b0d99ae5d3ca00dde88fb3e7073e8d679a6909d
kahenteikou <kokkiemouse@gmail.com>
2023-01-12 07:22:26 +0900

    2

--- FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
@@ -34,20 +34,20 @@ namespace FatNoder.ViewModels.Nodes
             Condition = new ValueNodeInputViewModel<HensuuUkewatashi?>
             {
                 Name = "Condition",
-                Label = Properties.Resources.IfNodeViewModel_ConditionLabel,
+                Label = Properties.Resources.ForNodeViewModel_ConditionLabel,
                 MaxConnections = 1
             };
             DefineFor = new ValueListNodeInputViewModel<StatementCls>
             {
                 Name = "Define",
-                Label = Properties.Resources.IfNodeViewModel_ThenLabel,
+                Label = Properties.Resources.ForNodeViewModel_DefineLabel,
                 MaxConnections = 1,
                 PortPosition = PortPosition.Right
             };
             OutFor = new ValueListNodeInputViewModel<StatementCls>
             {
                 Name = "Then",
-                Label = Properties.Resources.IfNodeViewModel_ElseLabel,
+                Label = Properties.Resources.ForNodeViewModel_ThenLabel,
                 MaxConnections = 1,
                 PortPosition = PortPosition.Right
             };
