8c208ebe3b324ebc47be9a29aedd78fd3e1d2b05
kahenteikou <kokkiemouse@gmail.com>
2023-01-11 23:28:08 +0900

    ren

--- FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace FatNoder.ViewModels
+namespace FatNoder.ViewModels.Nodes
 {
     public class ForNodeViewModel
     {
