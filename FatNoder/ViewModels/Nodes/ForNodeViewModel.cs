4a6794656d5eab96e75d2506f5ba59b0526da4ae
kahenteikou <kokkiemouse@gmail.com>
2023-01-11 23:36:41 +0900

    X

--- FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
+++ FatNoder/ViewModels/Nodes/ForNodeViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using NodeAyanoVMLibs.ViewModels.Nodes;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,8 @@ using System.Threading.Tasks;
 
 namespace FatNoder.ViewModels.Nodes
 {
-    public partial class ForNodeViewModel
+    public partial class ForNodeViewModel:StatementNodeViewModelBase,INodeViewModelBase
     {
+
     }
 }
