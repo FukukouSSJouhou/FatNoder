d2366a9991cb90575640d29a0fbdddd60476de9a
kahenteikou <kokkiemouse@gmail.com>
2023-01-15 16:06:49 +0900

    added 2

--- NodeAyano/Model/Enumerator/NodeModelEnumerator_ExSave.cs
+++ NodeAyano/Model/Enumerator/NodeModelEnumerator_ExSave.cs
@@ -1,5 +1,6 @@
 ﻿using FatNoder.Serializer.Node.Xml;
 using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,7 +8,80 @@ using System.Threading.Tasks;
 
 namespace NodeAyano.Model.Enumerator
 {
+    /// <summary>
+    /// NodeModelのEnumerator!(保存用)
+    /// </summary>
     public class NodeModelEnumerator_ExSave: IEnumerator<XML_NodeModel>
     {
+        private XML_NodeModel _Current;
+        private XML_NodeModel _root;
+        private IEnumerable<XML_NodeModel> _nodes;
+        /// <summary>
+        /// 現在の要素を取得
+        /// </summary>
+        public XML_NodeModel Current
+        {
+            get
+            {
+                return _Current;
+            }
+        }
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="root">root要素</param>
+        /// <param name="nodes">nodeたち</param>
+        public NodeModelEnumerator_ExSave(XML_NodeModel root, IEnumerable<XML_NodeModel> nodes)
+        {
+            _Current = null;
+            _root = root;
+            _nodes = nodes;
+        }
+
+        object IEnumerator.Current => throw new NotImplementedException();
+
+        /// <inheritdoc/>
+        public void Dispose()
+        {
+
+        }
+        /// <summary>
+        /// イテレータを次に進める
+        /// </summary>
+        /// <returns>成功したか否か</returns>
+        public bool MoveNext()
+        {
+
+            if (_Current == null)
+            {
+                _Current = _root;
+                return true;
+            }
+            else
+            {
+                foreach (XMLNodeInputStatement xns in _Current.InputStates)
+                {
+                    foreach (Guid uuidkun in xns.States)
+                    {
+                        foreach (XML_NodeModel nm in _nodes.Where(d =>
+                        {
+                            return d.UUID == uuidkun;
+                        }))
+                        {
+                            _Current = nm;
+                            return true;
+                        }
+                    }
+                }
+            }
+            return false;
+        }
+        /// <summary>
+        /// リセット
+        /// </summary>
+        public void Reset()
+        {
+            _Current = null;
+        }
     }
 }
